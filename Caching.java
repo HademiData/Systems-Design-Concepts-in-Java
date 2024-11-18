f23d025cb9094630bf704426681a898656b05b4c
HademiData <adewale.afolabi180221005@st.lasu.edu.ng>
2024-11-18 15:34:43 +0100

    Systems Design

---
+++ Caching.java
@@ -0,0 +1,27 @@
+/*
+ * Caching is a process of storing frequently accessed data in a 
+ * temporary storage layer (cache) to reduce latency and speed 
+ * up data retrieval.
+  Example: A web application can cache user data to avoid repeatedly 
+  querying a database.
+ */
+
+ import java.util.HashMap;
+
+class Caching {
+    private HashMap<String, String> cache = new HashMap<>();
+
+    public String getFromCache(String key) {
+        return cache.getOrDefault(key, "Data not found in cache");
+    }
+
+    public void addToCache(String key, String value) {
+        cache.put(key, value);
+    }
+
+    public static void main(String[] args) {
+        Caching cache = new Caching();
+        cache.addToCache("user1", "John Doe");
+        System.out.println(cache.getFromCache("user1")); // Output: John Doe
+    }
+}
