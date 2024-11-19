90d02744480919f849643146d4ba43c5b70481a8
HademiData <adewale.afolabi180221005@st.lasu.edu.ng>
2024-11-19 08:31:39 +0100

    Systems Design

--- LoadBalancer.java
+++ LoadBalancer.java
@@ -1,3 +1,39 @@
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class LoadBalancer {
     
+    //Distributes incoming traffic across multiple servers to
+    // ensure no single server is overwhelmed, improving performance
+    // and reliability.
+
+    class LoadBalan {
+        private List<String> servers = new ArrayList<>();
+        private int currentIndex = -1;
+
+        public LoadBalan() {
+            servers.add("Server1");
+            servers.add("Server2");
+            servers.add("Server3");
+        }
+
+        public String getServer() {
+            currentIndex = (currentIndex + 1) % servers.size();
+            return servers.get(currentIndex);
+        }
+
+        public static void main(String[] args) {
+
+            //creatin a new instance for the parent class
+            LoadBalancer loadBalancer = new LoadBalancer();
+
+            LoadBalan lb = loadBalancer.new LoadBalan();
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Request directed to: " + lb.getServer());
+            }
+        }
+    }
+
+
 }
